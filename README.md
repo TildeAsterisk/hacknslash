1e49678a96fb0afd44966cec429f5694118ab125
Tilde Asterisk <tildeAsterisk@gmail.com>
2026-02-22 17:09:25 +0000

    Setting up game. - Initialised word/map object heriarchy prototype - Set up Player 'scene', with CJ 3d model lol. - Third Person character controller player script

---
+++ README.md
@@ -0,0 +1,2 @@
+# Roguelike, Hack and Slash, Fast Paced, Endless
+Fight as many as you can, stay alive as long as you can, earn upgrades.
---
+++ scripts/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody3D
+
+@onready var camera_mount: Node3D = $camera_mount
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const sensitivity_x = 0.5
+const sensitivity_y = 0.25
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x)*sensitivity_x)
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y)*sensitivity_y)
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
